e954d0757ea24be944a2bf8d5bd26624c7106fe4
CorentinTrt <corentintruffaut@gmail.com>
2022-11-14 11:36:26 -0500

    keymaps.lua - change keybinds for moving lines + switch / resize windows

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -3,6 +3,8 @@ local keymap = vim.keymap
 -- [ Move lines ]
 keymap.set('n', '<C-j>', ':m .+1<CR>')
 keymap.set('n', '<C-k>', ':m .-2<CR>')
+keymap.set('v', '<C-j>', ":m '>+1<CR>gv=gv")
+keymap.set('v', '<C-k>', ":m '<-2<CR>gv=gv")
 
 -- [ Don't yank when x ]
 keymap.set('n', 'x', '"_x')
@@ -25,11 +27,17 @@ keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
 -- [ Windows ]
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
 
-keymap.set('n', '<Space>', '<C-w>w')
+-- keymap.set('n', '<Space>', '<C-w>w')
 keymap.set('', 'sk', '<C-w>k')
-keymap.set('', 'sl', '<C-w>l')
+keymap.set('n', '<Space>', '<C-w>l')
 keymap.set('', 'sj', '<C-w>j')
-keymap.set('', 'sh', '<C-w>h')
+keymap.set('n', '<S-Space>', '<C-w>h')
 
-keymap.set('n', '<C-w><left>', '<C-w><')
-keymap.set('n', '<C-w><right>', '<C-w>>')
+keymap.set('n', 's<left>', '<C-w><')
+keymap.set('n', 's<right>', '<C-w>>')
+
+-- [ Common keys ]
+keymap.set('n', ';p', ':Prettier<CR>')
+keymap.set('n', ';l', ';p :w')
+keymap.set('n', ';nn', ':noh<CR>')
+
